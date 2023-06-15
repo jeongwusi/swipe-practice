7a5d9f3acd61580c02636cc887e7dd6571440903
jeongwusi <jeongwusi@gmail.com>
2023-06-15 12:28:58 +0900

    ✨ feat: 좌우 기준으로 마우스가 움직이는 대로 카드의 위치가 변하는 기능 추가

--- src/components/App/swiper/card/index.tsx
+++ src/components/App/swiper/card/index.tsx
@@ -2,12 +2,21 @@ import styles from "./index.module.css";
 
 interface CardProps {
   pictures: { url: string }[];
+  currentIndex: number;
+  cardIndex: number;
 }
-const Card = ({ pictures }: CardProps) => {
+
+const Card = ({ pictures, currentIndex, cardIndex }: CardProps) => {
+  const isActive = currentIndex === cardIndex;
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} ${isActive ? styles.active : ""}`}>
       {pictures.map((picture, index) => (
-        <img key={index} src={picture.url} alt="sourceByUnsplash" />
+        <img
+          key={index}
+          src={picture.url}
+          alt="sourceByUnsplash"
+          style={{ display: isActive ? "block" : "none" }}
+        />
       ))}
     </div>
   );
--- src/components/App/swiper/index.tsx
+++ src/components/App/swiper/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import styles from "./index.module.css";
 import Card from "./card";
 
@@ -11,9 +11,62 @@ const Swiper = () => {
       url: "https://source.unsplash.com/random/1000x1000/?shoes",
     },
   ]);
+  const containerRef = useRef(null);
+  const [currentCardIndex, setCurrentCardIndex] = useState(0);
+  const [startX, setStartX] = useState(0);
+  const [isSwiping, setIsSwiping] = useState(false);
+
+  const handleTouchStart = (event: React.MouseEvent | React.TouchEvent) => {
+    const startX =
+      "touches" in event ? event.touches[0].clientX : event.clientX;
+    setStartX(startX);
+    setIsSwiping(true);
+  };
+
+  const handleTouchMove = (event: React.MouseEvent | React.TouchEvent) => {
+    if (!isSwiping) return;
+    const currentX =
+      "touches" in event ? event.touches[0].clientX : event.clientX;
+    const diffX = currentX - startX;
+    if (containerRef.current !== null) {
+      (
+        containerRef.current as HTMLElement
+      ).style.transform = `translateX(${diffX}px)`;
+    }
+  };
+
+  const handleTouchEnd = (event: React.MouseEvent | React.TouchEvent) => {
+    setIsSwiping(false);
+    if (containerRef.current !== null) {
+      (containerRef.current as HTMLElement).style.transform = "translateX(0)";
+    }
+    const threshold = 100;
+    const diffX =
+      startX - ("touches" in event ? event.touches[0].clientX : event.clientX);
+
+    if (diffX > threshold && currentCardIndex < pictures.length - 1) {
+      setCurrentCardIndex(currentCardIndex + 1);
+    } else if (diffX < -threshold && currentCardIndex > 0) {
+      setCurrentCardIndex(currentCardIndex - 1);
+    }
+  };
+
   return (
-    <div className={styles.container}>
-      <Card pictures={pictures} />
+    <div
+      className={styles.container}
+      ref={containerRef}
+      onMouseDown={handleTouchStart}
+      onMouseMove={handleTouchMove}
+      onMouseUp={handleTouchEnd}
+    >
+      {pictures.map((_, index) => (
+        <Card
+          key={index}
+          pictures={pictures}
+          currentIndex={currentCardIndex}
+          cardIndex={index}
+        />
+      ))}
     </div>
   );
 };
